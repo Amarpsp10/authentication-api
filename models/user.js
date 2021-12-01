c3a542bc60938878a2b81599392ad14941f77739
Amar Preet Singh <amarpsp10@gmail.com>
2021-12-01 22:16:07 +0530

    user and verify token schema added

--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
-import mongoos from "mongoose";
+const mongoos = require("mongoose");
 
 const UserSchema = new mongoos.Schema({
     email : {type:String, required:true, unique:true},
-    username : {type:String, required:true, unique:true},
-    password : {type:String, required:true},
-    name : {type:String, required:false},
+    username: {type:String, required:true, unique:true},
+    password : {type:String, required:true, unique:false},
+    name : {type:String, required:true, unique:false},
+    verified: {type:Boolean, default:false, required:false},
     gender : {type:String, required:false},
     dob : {type:Date, required:false}
 },{collection: 'users'})
 
 const User = mongoos.model('UserSchema',UserSchema)
 
-export default User
+module.exports = User
