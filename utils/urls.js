f88aac75ea0f8d338d77a3c6301072d9863b4a4f
Amar Preet Singh <68786618+Amarpsp10@users.noreply.github.com>
2021-12-03 19:44:11 +0530

    updated web url via github

--- utils/urls.js
+++ utils/urls.js
@@ -1,4 +1,4 @@
-const WebUrl = process.env.NODE_ENV==='development'? 'http://localhost:8080' : 'https://authentication-example-api.herokuapp.com'
+const WebUrl = process.env.NODE_ENV==='development'? 'http://localhost:8080' : 'https://react-auth-example.herokuapp.com'
 
 const Urls = {WebUrl}
 
